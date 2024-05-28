2c41f30dae37b381af6f62370f04f4e3ba505925
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-05-29 02:50:19 +0600

    font-poppins-setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,13 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <h3 className="text-4xl">This is Home.</h3>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h3 className="text-4xl font-poppins">This is Home.</h3>
         </div>
     );
 };
