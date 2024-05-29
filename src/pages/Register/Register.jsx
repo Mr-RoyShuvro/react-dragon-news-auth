50f7d635d8722a726e1ed981d7061eaa8364ca45
Mr-RoyShuvro <shuvroroyshuvo@gmail.com>
2024-05-30 01:50:21 +0600

    added login and sign up page

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const Register = () => {
+
+    const { createUser } = useContext(AuthContext);
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = new FormData(e.currentTarget);
+        const name = form.get('name');
+        const photoURL = form.get('photo');
+        const email = form.get('email');
+        const password = form.get('password');
+        console.log(name, photoURL, email, password);
+
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error =>{
+                console.error(error);
+            })
+    }
+
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div className="hero mt-14">
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 pt-10 ">
+                    <h3 className="text-3xl font-semibold text-center">Register your account</h3>
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Photo URL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="Password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                    </form>
+                    <p className="text-center pb-14 text-base">Already Have An Account? <Link className="text-red-500 font-semibold" to="/login">Login</Link></p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
